b66a53f06fd710039ea87b33f629404acebdce0f
Rahul Prasad <rahulprasad1399@gmail.com>
2025-08-03 17:38:25 +0530

    initial commit

---
+++ firstProgram.py
@@ -0,0 +1,18 @@
+print("Hello world")
+print(100)
+print(100+20)
+
+name = 'Rahul'
+age = 24
+hello = True
+print(type(name))
+print(type(age))
+
+#   hii i'm a comment
+
+#   relational operators
+a=50
+b=50
+print(a==b)
+print(a!=b)
+
